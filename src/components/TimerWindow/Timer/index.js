55744dd075ddf718d3f2e088a3e55bfbe878a4d2
Joel Hoelting Desktop <joelhoelting@protonmail.com>
2018-01-19 19:15:17 -0500

    Added a Parsing Method to Add Second 0 Under 10 Minutes/10 Seconds

--- src/components/TimerWindow/Timer/index.js
+++ src/components/TimerWindow/Timer/index.js
@@ -3,10 +3,19 @@ import React from 'react';
 import './Timer.css';
 
 const Timer = (props) => {
+  function parseTime(time) {
+    if (time < 10) {
+      return '0' + time;
+    } else {
+      return time;
+    }
+  }
+
   const {time} = props;
+
   return (
     <div className="timer">
-      <h1>{time.minutes}:{time.seconds}</h1>
+      <h1>{parseTime(time.minutes)}:{parseTime(time.seconds)}</h1>
     </div>
   );
 };
