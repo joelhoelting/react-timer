fdb443fa02e4ee68960224bda92c32d5687c6441
Joel Hoelting <joelhoelting@protonmail.com>
2018-01-23 12:13:59 -0500

    Remove WebFontLoader

--- src/index.js
+++ src/index.js
@@ -2,16 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import 'normalize.css';
-import WebFont from 'webfontloader';
 
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-
-WebFont.load({
-  google: {
-    families: ['Anonymous Pro:700', 'sans-serif']
-  }
-});
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
