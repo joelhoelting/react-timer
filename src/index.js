6a2c48feee49bf8e0fc2a6b4d7b50f5fe4f3f5c1
Joel Hoelting <joelhoelting@protonmail.com>
2018-01-23 12:08:51 -0500

    Added Webfont Loader

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import 'normalize.css';
+import WebFont from 'webfontloader';
+
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+
+WebFont.load({
+  google: {
+    families: ['Anonymous Pro:700', 'sans-serif']
+  }
+});
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
